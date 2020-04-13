c163a70e951402ddfba2664f842e9ce7ead46d13
wjvnsej <wjvnsej@hanmail.net>
2020-04-13 09:45:58 +0900

    git-bash로 세팅 후 push

--- src/chat2/MultiServer.java
+++ src/chat2/MultiServer.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+ 
 public class MultiServer {
 
 	public static void main(String[] args) {
